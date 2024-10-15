33fe8b18f64323a4d94b133e23edce089e1385cd
fnaja <fnaja688@gmail.com>
2024-10-15 11:43:27 +0530

    15/10/2024

--- lib/Task1.dart
+++ lib/Task1.dart
@@ -1,135 +1,103 @@
 import 'package:car_rent/Task2.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Task1 extends StatefulWidget {
-  const Task1({super.key});
+
+class Task_1 extends StatefulWidget {
+  const Task_1({super.key});
 
   @override
-  State<Task1> createState() => _Task1State();
+  State<Task_1> createState() => _Task_1State();
 }
 
-class _Task1State extends State<Task1> {
+class _Task_1State extends State<Task_1> {
+  final form_key = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 100.h, left: 20.w, right: 20.r),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.r)),
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: Colors.black,
+      body: SingleChildScrollView(
+        child: Form(
+          key: form_key,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 200, left: 30, right: 30),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty username";
+                    }
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Username",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
                 ),
-                prefixIconColor: Colors.white,
-                suffixIconColor: Colors.pink,
-                hintText: "Enter a name",
-                filled: true,
               ),
-            ),
-          ),
-          Column(
-            children: [
               Padding(
-                padding: EdgeInsets.only(left: 20.w, right: 20.r, top: 20.h),
-                child: TextField(
+                padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty phone number";
+                    }
+                  },
                   decoration: InputDecoration(
+                      hintText: "Phone number",
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.r)),
-                      prefixIcon: Icon(
-                        Icons.password,
-                        color: Colors.black,
-                      ),
-                      prefixIconColor: Colors.white,
-                      suffixIconColor: Colors.pink,
-                      hintText: "password",
-                      filled: true),
+                          borderRadius: BorderRadius.circular(10))),
                 ),
-              )
-            ],
-          ),
-          Column(
-            children: [
+              ),
               Padding(
-                padding: EdgeInsets.only(top: 20.h, left: 20.w, right: 20.r),
-                child: TextField(
+                padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Empty email";
+                    }
+                  },
                   decoration: InputDecoration(
+                      hintText: "E-mail",
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.r)),
-                      prefixIcon: Icon(
-                        Icons.phone,
-                        color: Colors.black,
-                      ),
-                      prefixIconColor: Colors.white,
-                      suffixIconColor: Colors.pink,
-                      hintText: "Enter a number",
-                      filled: true),
+                          borderRadius: BorderRadius.circular(10))),
                 ),
-              )
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 400.h),
-            child: InkWell(onTap: () {
-              Navigator.push(context,MaterialPageRoute(builder: (context) {
-                return Task2();
-
-              },) ,);
-            },
-              child: InkWell( onTap: () {
-                Navigator.push(context ,MaterialPageRoute(builder: (context) {
-                  return Task2();
-                },),);
-              },
-                child: Container(
-                  child: Center(
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 40),
+                child: ElevatedButton(
+                    onPressed: () {
+                      if (form_key.currentState!.validate()) {
+                        print("object");
+                      }
+                    },
                     child: Text(
                       "Submit",
                       style: TextStyle(
-                          color: Colors.white,
                           fontWeight: FontWeight.bold,
-                          fontSize: 20),
-                    ),
-                  ),
-                  width: 390.w,
-                  height: 60.h,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.r),
-                      color: Colors.blue),
-                ),
+                          fontSize: 20,
+                          color: Colors.black),
+                    )),
               ),
-            ),
-          ),
-          Row(
-            children: [
               Padding(
-                padding: EdgeInsets.only(top: 30.h, left: 30.w),
-                child: Container(
-                  child: Center(
+                padding: const EdgeInsets.only(top: 40),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) {
+                          return Task2();
+                        },
+                      ));
+                    },
                     child: Text(
-                      "Move to second page",
+                      "Next Page",
                       style: TextStyle(
-                          color: Colors.white,
                           fontWeight: FontWeight.bold,
-                          fontSize: 20),
-                    ),
-                  ),
-                  width: 390.w,
-                  height: 60.h,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.r),
-                      color: Colors.blue),
-                ),
-              ),
+                          fontSize: 20,
+                          color: Colors.black),
+                    )),
+              )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
